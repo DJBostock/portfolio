f975f52d9da702f1f0f3da6717f5a1bac89373dd
DJBostock <douglasjbostock@gmail.com>
2022-01-10 12:00:00 -0600

    add code for article page to be able to take a query string and select an article based on it.

---
+++ article.php
@@ -0,0 +1,46 @@
+<?php
+
+if(isset($_GET['id']) && is_numeric($_GET['id'])) {
+    $id = $_GET['id'];
+} else {
+    $id = 0;
+}
+
+include("./includes/db_credentials.php");
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if (mysqli_connect_error()) {
+    echo "connection error";
+    exit;
+} else {
+    $sql = "SELECT * FROM article WHERE id={$id};";
+    $response = mysqli_query($conn, $sql);
+    $result = mysqli_fetch_assoc($response);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Document</title>
+
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <?php if ($result === null): ?>
+        <p>Article not found.</p>
+    <?php else: ?>
+        <?= $result['title']; ?>
+        <br>
+        <?= $result['content']; ?>
+    <?php endif; ?>
+    
+    <script src="./js/script.js"></script>
+</body>
+</html>
