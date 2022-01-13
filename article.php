3003df436c91719be0bd054e3c013108aa658e22
DJBostock <douglasjbostock@gmail.com>
2022-01-13 12:00:00 -0600

    reduce duplicate code for the head element. create dynamic title.

--- article.php
+++ article.php
@@ -17,18 +17,14 @@ if (mysqli_connect_error()) {
     $result = mysqli_fetch_assoc($response);
 }
 
+$title = $result['title'];
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>Document</title>
-
-    <link rel="stylesheet" href="./css/style.css">
+    <?php include("./includes/head.php"); ?>
 </head>
 <body>
     <?php if ($result === null): ?>
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+$title = "Home";
+
 include("./includes/db_connection.php");
 
 if (mysqli_connect_error()) {
@@ -16,13 +18,7 @@ if (mysqli_connect_error()) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>Document</title>
-
-    <link rel="stylesheet" href="./css/style.css">
+    <?php include("./includes/head.php"); ?>
 </head>
 <body>
     <?php if (empty($result)): ?>
