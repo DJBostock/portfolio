fc22797880f42cc7e62c1cc048427c02a6bbdaa3
DJBostock <douglasjbostock@gmail.com>
2022-01-19 12:00:00 -0600

    start new article page.

---
+++ new_article.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include("./includes/head.php"); ?>
+</head>
+<body>
+    <nav>
+        <?php include("./includes/nav.php"); ?>
+    </nav>    
+    <script src="./js/script.js"></script>
+</body>
+</html>
