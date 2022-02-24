63c21d3fad80ff71548550c863f05680768212c9
DJBostock <douglasjbostock@gmail.com>
2022-02-24 12:00:00 -0600

    start about me page

---
+++ about.php
@@ -0,0 +1,35 @@
+<?php
+
+$title = "About Me";
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <?php include("./includes/head.php"); ?>
+</head>
+
+<body>
+    <header>
+        <?php include("./includes/header.php"); ?>
+    </header>
+
+    <nav>
+        <?php include("./includes/nav.php"); ?>
+    </nav>
+
+    <article>
+        <p>Under construction...</p>
+    </article>
+
+    <footer>
+        <?php include("./includes/footer.php"); ?>
+    </footer>
+
+    <script src="./js/script.js"></script>
+</body>
+
+</html>
--- includes/nav.php
+++ includes/nav.php
@@ -1,4 +1,5 @@
 <ul class="nav_ul">
     <li><a href="index.php">Home</a></li>
+    <li><a href="about.php">About Me</a></li>
     <li><a href="blog.php">Blog</a></li>
 </ul>
