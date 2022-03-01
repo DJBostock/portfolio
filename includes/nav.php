fce32986e2b5cd4b343cd360801b6546bda8a7fc
DJBostock <douglasjbostock@gmail.com>
2022-03-01 12:00:00 -0600

    start work on login page

--- includes/nav.php
+++ includes/nav.php
@@ -2,4 +2,5 @@
     <li><a href="index.php">Home</a></li>
     <li><a href="about.php">About Me</a></li>
     <li><a href="blog.php">Blog</a></li>
+    <li><a href="login.php">Login</a></li>
 </ul>
--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 
-$title = "About Me";
+$title = "Login";
 
 ?>
 
