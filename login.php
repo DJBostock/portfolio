b67be55df9c0f683217988429598cf86bb788819
DJBostock <douglasjbostock@gmail.com>
2022-03-03 12:00:00 -0600

    implement basic authentication

--- login.php
+++ login.php
@@ -2,9 +2,60 @@
 
 session_start();
 
-$_SESSION['is_logged_in'] = true;
-
 include("./includes/article_functions.php");
-redirect("/index.php");
+include("./includes/db_credentials.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if ($username === $my_username && $password === $my_password) {
+        $_SESSION['is_logged_in'] = true;
+        redirect("/blog.php");
+        exit;
+    }
+}
+
+$title = "Login";
 
 ?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <?php include("./includes/head.php"); ?>
+</head>
+
+<body>
+    <header>
+        <?php include("./includes/header.php"); ?>
+    </header>
+
+    <nav>
+        <?php include("./includes/nav.php"); ?>
+    </nav>
+
+    <article>
+        <form class="login_form" method="post" action="login.php">
+            <div>
+                <label for="username">Username:</label>
+                <input type="text" name="username" id="username">
+            </div>
+            <div>
+                <label for="password">Password:</label>
+                <input type="password" name="password" id="password">
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    </article>
+
+    <footer>
+        <?php include("./includes/footer.php"); ?>
+    </footer>
+
+    <script src="./js/script.js"></script>
+</body>
+
+</html>
